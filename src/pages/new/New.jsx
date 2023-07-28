d7483dc74a4629bfcbc7ce7f86743828e379eb71
sanjayguwaju <sanjayguwaju@gmail.com>
2023-07-28 14:01:24 +0545

    added: state to track user login state and * important field marked.

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -8,14 +8,27 @@ const New = ({ inputs, title }) => {
   const [file, setFile] = useState("");
   const [formData, setFormData] = useState({});
   const [isSubmitted, setIsSubmitted] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [isError, setIsError] = useState(false);
   const dispatch = useDispatch();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const userData = { ...formData, img: file };
     console.log(userData)
-    dispatch(addUser(userData));
-    setIsSubmitted(true);
+    try {
+      setIsSubmitted(true);
+      await dispatch(addUser(userData));
+      setIsSuccess(true);
+      setFormData({});
+      setFile("");
+      setIsSubmitted(false);
+      setTimeout(() => {
+        setIsSuccess(false);
+      }, 2000);
+    } catch (error) {
+      setIsError(true);
+    }
   };
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -54,13 +67,14 @@ const New = ({ inputs, title }) => {
               </div>
 
               <div className="formInput">
-                  <label>Username</label>
+                  <label>Username *</label>
                   <input
                     type="text"
                     name="username"
                     placeholder="Enter your username..."
                     onChange={handleInputChange}
                     required
+                    value={formData.username || ''}
                   />
               </div>
               <div className="formInput">
@@ -70,6 +84,7 @@ const New = ({ inputs, title }) => {
                     name="firstname"
                     placeholder="Enter your firstname..."
                     onChange={handleInputChange}
+                    value={formData.firstname || ''}
                   />
               </div>
               <div className="formInput">
@@ -79,29 +94,34 @@ const New = ({ inputs, title }) => {
                     name="lastname"
                     placeholder="Enter your lastname..."
                     onChange={handleInputChange}
+                    value={formData.lastname || ''}
                   />
               </div>
               <div className="formInput">
-                  <label>Email</label>
+                  <label>Email *</label>
                   <input
                     type="text"
                     name="email"
                     placeholder="Enter your email..."
                     onChange={handleInputChange}
                     required
+                    value={formData.email || ''}
                   />
               </div>
               <div className="formInput">
-                  <label>Password</label>
+                  <label>Password *</label>
                   <input
                     type="text"
                     name="password"
                     placeholder="Enter your password..."
                     onChange={handleInputChange}
                     required
+                    value={formData.password || ''}
                   />
-              </div>              <button type="submit">
-                {isSubmitted ? "Submitted Successfully" : "Submit Add User"}
+              </div>
+              {isError && <p>Server Error. Please try again.</p>}
+              <button type="submit">
+                {isSubmitted ? "Submitting..." : isSuccess ? "Submitted successfully!" : "Submit Add User"}
               </button>
             </form>
           </div>
